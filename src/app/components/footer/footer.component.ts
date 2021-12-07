177cb809ee424ff891d0ab8740d16717ea726293
Adriana Orellana <adrianaorellana34@gmail.com>
2021-12-07 11:16:17 -0400

    Add config file

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import config from '../../config.json';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  acronym: string = config.organizationData.acronym;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  getCurrentYear(): number {
+    return new Date().getFullYear();
   }
-
 }
