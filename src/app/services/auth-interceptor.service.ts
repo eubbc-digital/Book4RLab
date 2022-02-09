7617fc4c6032a4afbf60aa2b02c9771c99e55b3f
Adriana Orellana <adrianaorellana34@gmail.com>
2022-02-09 12:04:24 -0400

    Add auth interceptor service

---
+++ src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import config from '../config.json';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token: string = config.token;
+
+    let request = req;
+
+    if (token) {
+      request = req.clone({
+        setHeaders: {
+          authorization: `token ${token}`,
+        },
+      });
+    }
+    return next.handle(request).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 401) {
+          window.location.href = config.loginUrl;
+        }
+
+        return throwError(err);
+      })
+    );
+  }
+}
