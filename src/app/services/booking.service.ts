9039531e23543c3c2b7b3a12931ef28062161e5c
Adriana Orellana <adrianaorellana34@gmail.com>
2022-02-09 15:50:56 -0400

    Add booking service and interface

---
+++ src/app/services/booking.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import config from '../config.json';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookingService {
+  private url: string = `${config.api.baseUrl}${config.api.booking}`;
+
+  constructor(private http: HttpClient) {}
+
+  getBookingListByKitId(kitId: number): Observable<any> {
+    let params = new HttpParams().set('kit', kitId);
+
+    return this.http.get<any>(this.url, { params });
+  }
+}
