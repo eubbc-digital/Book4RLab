d9282aa989c5f000a9049459ea07122caab6ae7b
Adriana Orellana <adrianaorellana34@gmail.com>
2022-05-19 12:39:11 -0400

    Add country field to user model

--- app/core/models.py
+++ app/core/models.py
@@ -29,6 +29,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    country = models.CharField(max_length=10)
     
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
