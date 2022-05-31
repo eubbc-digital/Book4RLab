b159d612bdf7dfb3e0370a09dbe18645fa75e36d
Adriana Orellana <adrianaorellana34@gmail.com>
2022-05-31 15:36:34 -0400

    Update access component color

--- src/app/components/access/access.component.ts
+++ src/app/components/access/access.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import config from '../../config.json';
 
 @Component({
   selector: 'app-access',
   templateUrl: './access.component.html',
-  styleUrls: ['./access.component.css']
+  styleUrls: ['./access.component.css'],
 })
 export class AccessComponent implements OnInit {
-
-  constructor() { }
+  title: string = '';
+  constructor() {}
 
   ngOnInit(): void {
+    this.title = config.appInformation.title;
   }
-
 }
