f1555b5949589167f58c6bf65e616073d7e2ff19
AngelUrq <angel4441916@gmail.com>
2023-01-20 14:52:35 -0400

    Updated API Django versions and URLs

--- api/app/app/urls.py
+++ api/app/app/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('/', include('booking.urls')),
+    path('', include('booking.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
 ]
