e14dfdc0df13b1183d10a401ed6119c7f738b0fb
Andrea Sarmiento <anbesamo@gmail.com>
2023-07-20 11:24:51 -0400

    Add timezon in signup screen

--- ui/src/app/components/registration/registration.component.ts
+++ ui/src/app/components/registration/registration.component.ts
@@ -20,7 +20,10 @@ import { AuthService } from 'src/app/services/auth.service';
 import { User } from 'src/app/interfaces/user';
 import { Country } from 'src/app/interfaces/country';
 import { countries } from 'src/app/store/country-data-store';
+import { IanaTimezone } from 'src/app/interfaces/timezone';
+import { iana_timezones } from 'src/app/store/timezone-data-shortened-store';
 import { map, Observable, startWith } from 'rxjs';
+import moment from 'moment-timezone';
 
 @Component({
   selector: 'app-registration',
@@ -28,7 +31,13 @@ import { map, Observable, startWith } from 'rxjs';
   styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent implements OnInit {
+  
   countries: Country[] = countries;
+  
+  timezones: IanaTimezone[] = [];
+  // timezones : any[] = []
+  timezoneChosen : IanaTimezone = {group: '', timezone: '', label: ''};
+  timezoneDefault : IanaTimezone = {group: '', timezone: '', label: ''};
 
   hidePassword: boolean = true;
   hidePasswordConfirmation: boolean = true;
@@ -66,6 +75,12 @@ export class RegistrationComponent implements OnInit {
       map((value) => (typeof value === 'string' ? value : value.name)),
       map((name) => (name ? this.filterCountry(name) : this.countries.slice()))
     );
+    // this.getTimezones();
+    
+    this.timezoneDefault = this.getDefaultTimezone();
+    this.timezones = this.sortTimezones(iana_timezones);
+    this.timezoneChosen = this.timezoneDefault;
+    console.log(this.timezoneChosen);
   }
 
   matchValidator(matchTo: string, reverse?: boolean): ValidatorFn {
@@ -118,6 +133,7 @@ export class RegistrationComponent implements OnInit {
         email: this.registrationForm.controls['email'].value,
         password: this.passwordControl.value,
         country: this.countryControl.value.code,
+        time_zone: this.timezoneChosen.timezone,
       };
 
       this.authService.signUp(user).subscribe((response) => {
@@ -160,7 +176,43 @@ export class RegistrationComponent implements OnInit {
       country.name.toLowerCase().includes(filterValue)
     );
   }
+  getDefaultTimezone() {
+    const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    var utc;
+    var hour = moment().tz(tz).utcOffset()/60;
+    if (hour > -1) { utc = "UTC+" + hour.toString() + ":00" }
+    else { utc = "UTC" + hour.toString() + ":00" }
+    return { timezone:tz, group:utc,label:"" };
+  }
+  getTimezones(){
+    
+    const timezonesNames = moment.tz.names();
+    const timezonesC = moment.tz.countries();
+    
+    var timezonesWithUTC:any[] = []
+    
+    timezonesNames.forEach(tz => {
+      var utc;
+      var hour = moment().tz(tz).utcOffset()/60;
+      if (hour > -1) { utc = "UTC+" + hour.toString(); }
+      else { utc = "UTC" + hour.toString(); }
+      console.log(tz);
+      console.log(utc);
+      timezonesWithUTC.push({name:tz,UTC:utc})
+    });
+    this.timezones = timezonesWithUTC;
 
+  }
+  sortTimezones(timezones:any[]){
+      timezones.sort( function( a:any, b:any ) {
+        a = a.timezone.toLowerCase();
+        b = b.timezone.toLowerCase();
+    
+        return a < b ? -1 : a > b ? 1 : 0;
+    });
+    return timezones;
+
+  }
   getCountryName(country: any) {
     return country ? country.name : null;
   }
--- ui/src/app/interfaces/user.ts
+++ ui/src/app/interfaces/user.ts
@@ -13,4 +13,6 @@ export interface User {
   last_name?: string;
   token?: string;
   country?: string;
+  time_zone?:string;
+
 }
