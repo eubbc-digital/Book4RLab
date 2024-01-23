b53f6ef2eb7a07c28273bb9c5f7b9edc6047fbb2
bpedrazaa <bpedrazaa@gmail.com>
2024-01-23 13:49:36 -0400

    Add api contributions to repository

--- booking_api/app/core/models.py
+++ booking_api/app/core/models.py
@@ -1,7 +1,7 @@
 """
 Copyright (c) Universidad Privada Boliviana (UPB) - EUBBC-Digital
 MIT License - See LICENSE file in the root directory
-Adriana Orellana, Angel Zenteno, Alex Villazon, Omar Ormachea
+Adriana Orellana, Angel Zenteno, Boris Pedraza, Alex Villazon, Omar Ormachea
 """
 
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
